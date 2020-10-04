195995ff167937a151309ee6032c75c7c1a3e0df
godbay10 <71583398+godbay10@users.noreply.github.com>
2020-10-04 21:40:59 +0900

    Add files via upload

---
+++ 퍼즐 맞추기/퍼즐_맞추기.py
@@ -0,0 +1,84 @@
+from bangtal import*
+import random
+
+setGameOption(GameOption.INVENTORY_BUTTON,False)
+setGameOption(GameOption.MESSAGE_BOX_BUTTON,False)
+
+scene=Scene("겨울왕국4", "Images/배경.png")
+
+def find_index(object):
+    for index in range(16):
+        if game_board[index]==object:return index
+
+def movable(index):
+    if index<0: return False
+    if index>15: return False
+    if index%4>0 and index-1==blank: return True
+    if index%4<3 and index+1==blank: return True
+    if index>3 and index-4==blank: return True
+    if index<12 and index+4==blank: return True
+    return False
+
+def move(index):
+    global blank
+    game_board[index].locate(scene,300+ 150 * (blank % 4), 470 -150 * (blank // 4))
+    game_board[blank].locate(scene,300+ 150 * (index % 4), 470 -150 * (index // 4))
+
+    game_board[index], game_board[blank] = game_board[blank], game_board[index]
+    blank=index
+
+def completed():
+    for index in range(16):
+        if game_board[index]!=init_board[index]: return False
+    return True
+
+delta=[-1,1,-4,4]
+
+def random_move():
+    while True:
+        index=blank+delta[random.randrange(4)]
+        if movable(index):break
+    move(index)
+
+
+class Piece(Object):
+    def __init__(self,file,scene,x,y):
+        super().__init__(file)
+        self.locate(scene,x,y)
+        self.show()
+    def onMouseAction(self, x, y, action):
+        index=find_index(self)
+        if movable(index):
+            move(index)
+
+            if completed():
+                showMessage("completed!")
+
+game_board=[]
+init_board=[]
+
+for index in range(16):
+    piece=Piece("Images/"+str(index+1)+".png",scene,300+ 150 * (index % 4), 470 -150 * (index // 4))
+
+    game_board.append(piece)
+    init_board.append(piece)
+
+blank=15
+game_board[blank].hide()
+
+count=5
+timer=Timer(1)
+
+def onTimeout():   
+    random_move()
+
+    global count
+    count=count-1
+    if count>0:
+        timer.set(0.1)
+        timer.start()
+timer.onTimeout=onTimeout
+
+timer.start()
+
+startGame(scene)
